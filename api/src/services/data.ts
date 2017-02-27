2a0bc11a5191fb4c2cf44918ff67ddb8c7eee649
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-02-27 18:47:29 +0200

    Adds data service

---
+++ api/src/services/data.ts
@@ -0,0 +1,24 @@
+export class DataService {
+
+    constructor() {
+
+    }
+
+    list(): Promise<any[]> {
+        return new Promise((resolve: Function, reject: Function) => {
+
+        });
+    }
+
+    create(title: string, description: string) {
+        return new Promise((resolve: Function, reject: Function) => {
+
+        });
+    }
+
+    find(id: string) {
+        return new Promise((resolve: Function, reject: Function) => {
+
+        });
+    }
+}
