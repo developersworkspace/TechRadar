b529d375fbc51d4cfbe7fd2c192d399a97217886
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-02-27 18:59:24 +0200

    update

--- api/src/services/data.ts
+++ api/src/services/data.ts
@@ -21,4 +21,30 @@ export class DataService {
 
         });
     }
+
+    private getInstanceOfItem(title: string, description: string, quadrant: string) {
+        return {
+            id: this.generateId(),
+            name: title,
+            quadrant: quadrant,
+            value: 0,
+            angle: this.generateAngle(),
+            description: description
+        };
+    }
+
+    private generateAngle() {
+        return Math.random() * 90
+    }
+
+    private generateId(): string {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + '-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + '-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + '-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + '-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+            + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
 }
