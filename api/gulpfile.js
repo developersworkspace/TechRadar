268bb61bbfa29430f6b37ae0efaa7707f049d0fe
Brendan.Huysamen <developersworkspace@gmail.com>
2017-03-03 08:26:09 +0200

    adds prod config

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -2,9 +2,11 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var ts = require('gulp-typescript');
+var rename = require("gulp-rename");
+var sequence = require('run-sequence');
 
-// Compile typescript files
-gulp.task('ts', ['clean'], function () {
+// Compiles typescript files
+gulp.task('compile:ts.dev', function () {
     return gulp
         .src(["./src/**/*.ts"], { base: './src' })
         .pipe(ts({ module: 'commonjs', target: 'es6', noImplicitAny: false, allowJs: true, allowUnreachableCode: true }))
@@ -12,10 +14,19 @@ gulp.task('ts', ['clean'], function () {
 });
 
 // Removes compiled js files
-gulp.task('clean', function () {
+gulp.task('clean:js', function () {
     return gulp
         .src([
             './src/**/*.js',
+        ], { read: false })
+        .pipe(clean())
+});
+
+
+// Removes compiled js files
+gulp.task('clean:dist', function () {
+    return gulp
+        .src([
             './dist'
         ], { read: false })
         .pipe(clean())
@@ -23,17 +34,33 @@ gulp.task('clean', function () {
 
 
 // Copies 'package.json' file to build directory
-gulp.task('build1', ['clean'], function () {
+gulp.task('copy:package.json', function () {
     return gulp
         .src('./package.json')
         .pipe(gulp.dest('./dist'));
 });
 
 
-// Compile typescript files
-gulp.task('build', ['build1'], function () {
+// Compiles typescript files
+gulp.task('compile:ts.prod', function () {
     return gulp
         .src(["./src/**/*.ts"], { base: './src' })
         .pipe(ts({ module: 'commonjs', target: 'es6', noImplicitAny: false, allowJs: true, allowUnreachableCode: true }))
         .pipe(gulp.dest('./dist'));
+});
+
+// Renames config file
+gulp.task('rename:config', function () {
+    return gulp.src('./dist/config.prod.js', { base: process.cwd() })
+        .pipe(rename('config.js'))
+        .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('build:dev', function (done) {
+    sequence('clean:js', 'compile:ts.dev', done);
+});
+
+gulp.task('build:prod', function (done) {
+    sequence('clean:dist', 'compile:ts.prod', 'copy:package.json', 'rename:config', done);
 });
